01c1899eb99ec0b8e515216b49e72999c4b80108
Noah Appelbaum <noahappelbaum@gmail.com>
2024-07-03 13:43:24 -0500

    adjusted imports and added user prop for displayed lifebox

--- app/game/[room].tsx
+++ app/game/[room].tsx
@@ -1,13 +1,13 @@
 import { Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
-import Lifebox from "@/components/game/lifebox";
+import Lifebox from "@/components/game/Lifebox";
 
 export default function Game() {
     const {room} = useLocalSearchParams<{room: string}>()
     return (
         <View>
             {/* TODO: Make starting life total adjustable */}
-            <Lifebox initLifeTotal={20} />
+            <Lifebox initLifeTotal={20} user={true}/>
             <Text>You are now in room {room}</Text>
         </View>
     )
